ce4c0894bcb1be3b446d8c675c6d363bca541ab8
Jmeier2715 <YOUR-EMAIL-ADDRESS>
2021-10-26 23:07:30 -0400

    third commit

--- js/app.js
+++ js/app.js
@@ -1,5 +1,3 @@
-
-
 // fetch(url)
 //   .then(response => response.json())
 //   .then(price => console.log(price))
@@ -12,11 +10,10 @@
   //need to create function that assigns a random two part output; one from an array [2, 3, 4, 5, 6, 7, 8, 9, 10, J, Q, K, A], and array [C, S, D, H]
   //on click function must return two values that fit the format of the api
   //money value must coincide w bet value
-var cardValue = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
 function randomValue(){
     return cardValue[Math.floor(Math.random()*cardValue.length)];
 }
-
+let cardValue = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
 var cardSuit = ['C', 'S', 'D', 'H']
 function randomSuit(){
     return cardSuit[Math.floor(Math.random()*cardSuit.length)];
@@ -25,8 +22,6 @@ function randomSuit(){
 function dealtHand (){
     return randomValue()+randomSuit();
 }
-console.log(dealtHand())
-console.log(dealtHand())
 
 let playerCardOne = document.querySelector('.playerCardOne')
 let playerCardTwo = document.querySelector('.playerCardTwo')
@@ -38,29 +33,79 @@ let communityCardThree = document.querySelector('.communityCardThree')
 let communityCardFour = document.querySelector('.communityCardFour')
 let communityCardFive = document.querySelector('.communityCardFive')
 
+// let bankroll = document.querySelector ('bankroll')
+// let moneyInTheBank = document.querySelector('myNumber')
+// function modifyMoney () {
+//     bankroll.innerHTML - moneyInTheBank
+// }
+// modifyMoney()
 
-function cardDraw () {
-    playerCardOne.innerText = dealtHand()
-    playerCardTwo.innerText = dealtHand()
-    dealerCardOne.innerText = dealtHand()
-    dealerCardTwo.innerText = dealtHand()
+// let checkFunctions = [
+//     function(){
+//         communityCardOne.innerText = dealtHand()
+//         communityCardTwo.innerText = dealtHand()
+//         communityCardThree.innerText = dealtHand()
+//     },
+//     function(){ 
+//         communityCardFour.innerText = dealtHand()
+//     },
+//     function(){
+//         communityCardFive.innerText = dealtHand()
+//     }
+// ];
+let money = 250
+let moneyCounter = document.querySelector ('.bankroll')
+
+let i = 0;
+window.onload = function obama(){
+    let btnBet = document.getElementsByClassName('bet')[0];
+    btnBet.onclick = 
+        function () {
+        betFunctions[i++ % betFunctions.length]();
+        console.log('vegetables')
+    }
+    
     
 }
-function theFlop (){
-    communityCardOne.innerText = dealtHand()
-    communityCardTwo.innerText = dealtHand()
-    communityCardThree.innerText = dealtHand()
-}
-function theTurn(){ 
-    communityCardFour.innerText = dealtHand()
-}
-function theRiver(){
-    communityCardFive.innerText = dealtHand()
-}
-cardDraw()
-theFlop()
-theTurn()
-theRiver()
+let betFunctions = [
+    function() {
+        playerCardOne.innerText = dealtHand()
+        playerCardTwo.innerText = dealtHand()
+        dealerCardOne.innerText = dealtHand()
+        dealerCardTwo.innerText = dealtHand()
+        moneyCounter.innerText = money
+    },
+    function(){
+        communityCardOne.innerText = dealtHand()
+        communityCardTwo.innerText = dealtHand()
+        communityCardThree.innerText = dealtHand()
+    },
+    function(){ 
+        communityCardFour.innerText = dealtHand()
+    },
+    function(){
+        communityCardFive.innerText = dealtHand()
+    },
+];
+
+
+
+
+
+
+// function theFlop (){
+    
+// }
+// function theTurn(){ 
+//     
+// }
+// function theRiver(){
+//     
+// }
+// cardDraw()
+// theFlop()
+// theTurn()
+// theRiver()
 
 //need to hide dealer cards until after the river function
 //need to include a rules popup button
@@ -70,4 +115,12 @@ theRiver()
 
 // console.log(url)
 
-// //assign 
+// array of functions
+// window.onload = function biden (){
+//     let btnCheck = document.getElementsByClassName('check')[0];
+//     btnCheck.onclick = function(){
+//         checkFunctions[i++ % checkFunctions.length]();
+//     }
+// }
+// function kamala (){
+//     let btnFold = document.getElement 
---
+++ pseudocode.md
@@ -0,0 +1,8 @@
+need to connect to the api 
+must concatenate the input value
+organize the css 
+create the money counter
+have the money counter interact with the betting value 
+
+
+need to stop the button interaction after one series, 
