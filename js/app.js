99faf48bebc443b87e01ab5d6dfcfd1de25a93ad
Jmeier2715 <YOUR-EMAIL-ADDRESS>
2021-10-28 02:25:45 -0400

    fourth commit

--- js/app.js
+++ js/app.js
@@ -1,28 +1,6 @@
-// fetch(url)
-//   .then(response => response.json())
-//   .then(price => console.log(price))
-//   console.log('meat')
-
-// // let cards = document.querySelector('#cards')
-// console.log('chicken')
-
-
-  //need to create function that assigns a random two part output; one from an array [2, 3, 4, 5, 6, 7, 8, 9, 10, J, Q, K, A], and array [C, S, D, H]
-  //on click function must return two values that fit the format of the api
-  //money value must coincide w bet value
-function randomValue(){
-    return cardValue[Math.floor(Math.random()*cardValue.length)];
-}
+let gameIsWon = true
 let cardValue = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
-var cardSuit = ['C', 'S', 'D', 'H']
-function randomSuit(){
-    return cardSuit[Math.floor(Math.random()*cardSuit.length)];
-}
-
-function dealtHand (){
-    return randomValue()+randomSuit();
-}
-
+let cardSuit = ['C', 'S', 'D', 'H']
 let playerCardOne = document.querySelector('.playerCardOne')
 let playerCardTwo = document.querySelector('.playerCardTwo')
 let dealerCardOne = document.querySelector('.dealerCardOne')
@@ -33,94 +11,114 @@ let communityCardThree = document.querySelector('.communityCardThree')
 let communityCardFour = document.querySelector('.communityCardFour')
 let communityCardFive = document.querySelector('.communityCardFive')
 
-// let bankroll = document.querySelector ('bankroll')
-// let moneyInTheBank = document.querySelector('myNumber')
-// function modifyMoney () {
-//     bankroll.innerHTML - moneyInTheBank
-// }
-// modifyMoney()
+//functions that controls card distribution
+function randomValue(){
+    return cardValue[Math.floor(Math.random()*cardValue.length)];
+}
+function randomSuit(){
+   return cardSuit[Math.floor(Math.random()*cardSuit.length)];
+}
+function dealtHand (){
+   return randomValue()+randomSuit();
+}
 
-// let checkFunctions = [
-//     function(){
-//         communityCardOne.innerText = dealtHand()
-//         communityCardTwo.innerText = dealtHand()
-//         communityCardThree.innerText = dealtHand()
-//     },
-//     function(){ 
-//         communityCardFour.innerText = dealtHand()
-//     },
-//     function(){
-//         communityCardFive.innerText = dealtHand()
-//     }
-// ];
-let money = 250
-let moneyCounter = document.querySelector ('.bankroll')
+let bankroll = document.querySelector('.bankroll')
+var input = document.querySelector('input')
+let i = 0
 
-let i = 0;
+
+function michelle () {
+    betFunctions[i++ % betFunctions.length]();
+    console.log('vegetables');
+};
 window.onload = function obama(){
     let btnBet = document.getElementsByClassName('bet')[0];
-    btnBet.onclick = 
-        function () {
-        betFunctions[i++ % betFunctions.length]();
-        console.log('vegetables')
+    btnBet.addEventListener('click', michelle)
+    // btnBet.addEventListener('click', myFunction)
+}
+
+let betFunctions = [
+    () => {
+        playerCardOne.innerText = dealtHand();
+        playerCardTwo.innerText = dealtHand();
+        dealerCardOne.innerText = dealtHand();
+        dealerCardTwo.innerText = dealtHand();
+        var x = document.getElementById("input").value;
+        document.getElementById("ante").innerHTML = `Ante: $${x}`;
+        bankroll.innerText = (Number.parseInt(bankroll.innerText))- (Number.parseInt(x))
+        console.log('dragons')
+        
+        
+
+    },
+    () => {
+        communityCardOne.innerText = dealtHand();
+        communityCardTwo.innerText = dealtHand();
+        communityCardThree.innerText = dealtHand();
+        var y = document.getElementById("input").value;
+        document.getElementById("preflop").innerHTML = `Pre-Flop: $${y}`;
+        bankroll.innerText = (Number.parseInt(bankroll.innerText))-(Number.parseInt(y))
+        console.log('twin dragons')
+        
+    },
+    () => {
+        communityCardFour.innerText = dealtHand();
+        var z = document.getElementById("input").value;
+        document.getElementById("flop").innerHTML = `Flop: $${z}`;
+        bankroll.innerText = (Number.parseInt(bankroll.innerText))-(Number.parseInt(z))
+        console.log('triple dragons')
+    },
+    () => {
+        communityCardFive.innerText = dealtHand();
+        var a = document.getElementById("input").value;
+        document.getElementById("turn").innerHTML = `Turn: $${a}`;
+        bankroll.innerText = (Number.parseInt(bankroll.innerText))-(Number.parseInt(a))
+        console.log('quad dragons')
+    },
+    () => {
+        var b = document.getElementById("input").value;
+        document.getElementById("river").innerHTML = `River: $${b}`;
+        bankroll.innerText = (Number.parseInt(bankroll.innerText))-(Number.parseInt(b))
+        console.log('quint dragons')
+
+        //check win (){
+            //
+        //}
+        return
+        //insert check win function here
+        //insert subtract or add balance here
+        //reveal the dealer's cards onclick here 
     }
     
-    
-}
-let betFunctions = [
-    function() {
-        playerCardOne.innerText = dealtHand()
-        playerCardTwo.innerText = dealtHand()
-        dealerCardOne.innerText = dealtHand()
-        dealerCardTwo.innerText = dealtHand()
-        moneyCounter.innerText = money
-    },
-    function(){
-        communityCardOne.innerText = dealtHand()
-        communityCardTwo.innerText = dealtHand()
-        communityCardThree.innerText = dealtHand()
-    },
-    function(){ 
-        communityCardFour.innerText = dealtHand()
-    },
-    function(){
-        communityCardFive.innerText = dealtHand()
-    },
 ];
-
-
-
-
-
-
-// function theFlop (){
-    
-// }
-// function theTurn(){ 
-//     
-// }
-// function theRiver(){
-//     
-// }
-// cardDraw()
-// theFlop()
-// theTurn()
-// theRiver()
-
+console.log(preflop.innerText)
 //need to hide dealer cards until after the river function
 //need to include a rules popup button
 //on click function the flop, the turn, 
 //if any of the cards are equal to each other, they have to be recycled 
 // let url = 'https://api.pokerapi.dev/v1/winner/texas_holdem?cc=['+deltHand+dealtHand+dealtHand+dealtHand+dealtHand+']&pc[]=&pc[]'
 
-// console.log(url)
 
-// array of functions
-// window.onload = function biden (){
-//     let btnCheck = document.getElementsByClassName('check')[0];
-//     btnCheck.onclick = function(){
-//         checkFunctions[i++ % checkFunctions.length]();
-//     }
+// let ante = document.getElementById('ante')
+// console.log(ante.innerHTML)
+
+// var money = (bankroll.innerText) - (ante.innerText)
+
+
+
+
+// };
+// function sumOfBets (){
+//     parseInt(x) + parseInt(y) + parseInt(z) + parseInt(a) + parseInt(b)
 // }
-// function kamala (){
-//     let btnFold = document.getElement 
+// var x = document.getElementById("input").value;
+// console.log(x)
+// var y = document.getElementById("input").value;
+// console.log(y)
+// var z = document.getElementById("input").value;
+// console.log(z)
+// var b = document.getElementById("input").value;
+// console.log(b)
+// var a = document.getElementById("input").value;
+// console.log(a)
+// // sumOfBets()
